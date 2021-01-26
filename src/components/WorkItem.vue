67a3b99cf1591cabb1b24ba191a63e4402ecd616
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 14:35:08 +0330

    WorkItem component styles

--- src/components/WorkItem.vue
+++ src/components/WorkItem.vue
@@ -1,4 +1,5 @@
 <template>
+<!-- <transition name=""> -->
   <div class="work-item">
     <span>{{ workItem.title }}</span>
     <select
@@ -6,14 +7,15 @@
       @change="changeState()"
       v-model="workItem.state"
     >
-      <option value="new">New</option>
-      <option value="active">Active</option>
-      <option value="pendedReview">Pended Review</option>
-      <option value="resolved">Resolved</option>
+      <option value="new">جدید</option>
+      <option value="active">فعال</option>
+      <option value="pendedReview">در انتظار بررسی</option>
+      <option value="resolved">بسته شده</option>
     </select>
 
-    <button @click.prevent="remove()">delete</button>
+    <button @click.prevent="remove()">حذف</button>
   </div>
+<!-- </transition> -->
 </template>
 
 <script>
@@ -41,10 +43,34 @@ export default {
 
 <style scoped>
 .work-item {
-  border: 1px solid darkgrey;
-  padding: 5px;
+  border: 1px solid #d5d5d5;
+  background-color: #ebe9e9;
+  margin: 10px;
+  padding: 10px;
+  box-shadow: 1px 2px 1px 2px #bdbcbc;
   border-radius: 3px;
   display: flex;
   flex-direction: column;
 }
+.work-item span {
+  font-size: 13px;
+  font-weight: bold;
+}
+.work-item select {
+  padding: 3px 1px;
+  border: 1px solid gray;
+  border-radius: 2px;
+  margin: 10px 0;
+  cursor: pointer;
+}
+.work-item button {
+  width: 50%;
+  cursor: pointer;
+  margin-top: 10px;
+  background-color: rgb(255, 114, 62);
+  color: white;
+  border: 1px solid gray;
+  border-radius: 3px;
+}
+
 </style>
