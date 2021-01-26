82291dae32813b87e675961b75d39458ec8f97cb
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 14:34:30 +0330

    add global styles

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,45 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: vazir;
+  src: url('~@/assets/font/Vazir-FD.woff2');
+}
+* {
+  font-family: vazir;
+  font-size: 12px;
+  box-sizing: border-box;
+}
+body {
+  background-color: #f0f0ff;
+  padding: 10px;
+}
 #app {
   display: flex;
   flex-direction: column;
+  margin: 0 auto;
+  width: 75%
 }
 .work-items-lists {
   display: flex;
+  margin-top: 10px;
+}
+
+@media only screen and (max-width: 1300px) {
+  #app {
+    width: 90%
+  }
+}
+
+
+@media only screen and (max-width: 1200px) {
+  #app {
+    width: 95%
+  }
+@media only screen and (max-width: 1000px) {
+  #app {
+    width: 100%
+  }
+}
 }
 </style>
