8ab582ccf29abdb5609a3e3a4f137607e0bd8377
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 11:35:09 +0330

    create WorkItemsList with static content

---
+++ src/components/WorkItemsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="list">
+      <WorkItem />
+      <WorkItem />
+      <WorkItem />
+      <WorkItem />
+  </div>
+</template>
+
+<script>
+import WorkItem from '@/components/WorkItem'
+
+export default {
+    components: {
+        WorkItem
+    }
+}
+</script>
+
+<style scoped>
+.list {
+    border-right: 1px solid black; 
+    border-left: 1px solid black;
+    padding: 5px;
+    background-color: #e6e4e4;
+    display: flex;
+    flex-direction: column;
+}
+</style>
