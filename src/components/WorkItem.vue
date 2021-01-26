3849e8efd566593d28371c1609401c2b73efd17e
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 11:13:17 +0330

    create WorkItem component with initial structure

---
+++ src/components/WorkItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="work-item">
+      <span>work item title</span>
+      <select name="workItemState">
+          <option value="new">New</option>
+          <option value="active">Active</option>
+          <option value="pendedReview">Pended Review</option>
+          <option value="resolved">Resolved</option>
+      </select>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.work-item {
+    border: 1px solid darkgrey;
+    padding: 5px;
+    border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+}
+</style>
