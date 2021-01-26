a24c48d267abccec92ae758d436144529aa97903
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 12:36:50 +0330

    CRUD actions and mutation

--- src/store/index.js
+++ src/store/index.js
@@ -13,10 +13,33 @@ export default new Vuex.Store({
       { id: uuidv4(), title: 'Users Page', state: 'resolved' }
     ]
   },
+
   mutations: {
+    setWorkItemState(state, updatedWorkItem) {
+      state.workItems.forEach(item => {
+        if (item.id === updatedWorkItem.id) Vue.set(item, 'state', updatedWorkItem.state)
+      })
+    },
+    setNewWorkItem(state, newWorkItem) {
+      state.workItems.push({ ...newWorkItem, id: uuidv4() });
+    },
+    removeWorkItem(state, id) {
+      state.workItems = state.workItems.filter(item => item.id !== id);
+    }
   },
+
   actions: {
+    changeWorkItemState({ commit }, updatedWorkItem) {
+      commit('setWorkItemState', updatedWorkItem);
+    },
+    addNewWorkItem({ commit }, newWorkItem) {
+      commit('setNewWorkItem', newWorkItem);
+    },
+    deleteWorkItem({ commit }, id) {
+      commit('removeWorkItem', id);
+    }
   },
+
   modules: {
   }
 })
