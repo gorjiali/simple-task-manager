b780dc3164c26d35a30945604049ef26c185b863
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 12:36:18 +0330

    add functionality to workItemsList

--- src/components/WorkItemsList.vue
+++ src/components/WorkItemsList.vue
@@ -1,29 +1,42 @@
 <template>
   <div class="list">
-      <WorkItem />
-      <WorkItem />
-      <WorkItem />
-      <WorkItem />
+    <WorkItem
+      v-for="item in items"
+      :key="item.title + Math.random()"
+      :item="item"
+    />
   </div>
 </template>
 
 <script>
-import WorkItem from '@/components/WorkItem'
+import WorkItem from "@/components/WorkItem";
 
 export default {
-    components: {
-        WorkItem
-    }
-}
+  props: {
+    type: String,
+  },
+
+  components: {
+    WorkItem,
+  },
+
+  computed: {
+    items() {
+      return this.$store.state.workItems.filter(
+        (item) => item.state === this.type
+      );
+    },
+  },
+};
 </script>
 
 <style scoped>
 .list {
-    border-right: 1px solid black; 
-    border-left: 1px solid black;
-    padding: 5px;
-    background-color: #e6e4e4;
-    display: flex;
-    flex-direction: column;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
+  padding: 5px;
+  background-color: #e6e4e4;
+  display: flex;
+  flex-direction: column;
 }
 </style>
