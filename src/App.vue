4a71524038708ecb31d0f10507b56ad314887155
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 12:35:33 +0330

    create AddWorkItem component and add to app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
   <div id="app">
-    <WorkItemsList />
-    <WorkItemsList />
-    <WorkItemsList />
-    <WorkItemsList />
+    <AddWorkItem />
+    <div class="work-items-lists">
+      <WorkItemsList type="new" />
+      <WorkItemsList type="active" />
+      <WorkItemsList type="pendedReview" />
+      <WorkItemsList type="resolved" />
+    </div>
   </div>
 </template>
 
 <script>
-import WorkItemsList from '@/components/WorkItemsList';
+import WorkItemsList from "@/components/WorkItemsList";
+import AddWorkItem from "@/components/AddWorkItem";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    WorkItemsList
-  }
-}
+    WorkItemsList,
+    AddWorkItem
+  },
+};
 </script>
 
 <style>
 #app {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+}
+.work-items-lists {
+  display: flex;
 }
 </style>
---
+++ src/components/AddWorkItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <input
+      type="text"
+      placeholder="Enter new work item title"
+      v-model="newWorkItem.title"
+    />
+    <button @click.prevent="add()" >add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newWorkItem: {
+          title: '',
+          state: 'new'
+      },
+    };
+  },
+
+  methods: {
+    add() {
+        if (this.newWorkItem.title === '') return alert('Enter title!!');
+        this.$store.dispatch('addNewWorkItem', this.newWorkItem);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
