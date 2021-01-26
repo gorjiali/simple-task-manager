43a7c9443b5dc835efb8f0f1c33569cffd19ee11
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 12:35:59 +0330

    add functionality to workItem

--- src/components/WorkItem.vue
+++ src/components/WorkItem.vue
@@ -1,27 +1,50 @@
 <template>
   <div class="work-item">
-      <span>work item title</span>
-      <select name="workItemState">
-          <option value="new">New</option>
-          <option value="active">Active</option>
-          <option value="pendedReview">Pended Review</option>
-          <option value="resolved">Resolved</option>
-      </select>
+    <span>{{ workItem.title }}</span>
+    <select
+      name="workItemState"
+      @change="changeState()"
+      v-model="workItem.state"
+    >
+      <option value="new">New</option>
+      <option value="active">Active</option>
+      <option value="pendedReview">Pended Review</option>
+      <option value="resolved">Resolved</option>
+    </select>
+
+    <button @click.prevent="remove()">delete</button>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    item: Object,
+  },
 
-}
+  data() {
+    return {
+      workItem: { ...this.item },
+    };
+  },
+
+  methods: {
+    changeState() {
+      this.$store.dispatch("changeWorkItemState", this.workItem);
+    },
+    remove() {
+        this.$store.dispatch("deleteWorkItem", this.workItem.id);
+    }
+  },
+};
 </script>
 
 <style scoped>
 .work-item {
-    border: 1px solid darkgrey;
-    padding: 5px;
-    border-radius: 3px;
-    display: flex;
-    flex-direction: column;
+  border: 1px solid darkgrey;
+  padding: 5px;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
