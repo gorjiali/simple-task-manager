47a20cba2ca17f95d674a2d9ff281c3b6def47c0
Ali Gorji <ag.aligorji@gmail.com>
2021-01-26 11:48:58 +0330

    add uuid package and use it for add id to workItems

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { v4 as uuidv4 } from 'uuid';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     workItems: [
-      { title: 'Refactor Livebook', state: 'new' },
-      { title: 'Login Page', state: 'active' },
-      { title: 'Dashboard Layout', state: 'pendedReview' },
-      { title: 'Users Page', state: 'resolved' }
+      { id: uuidv4(), title: 'Refactor Livebook', state: 'new' },
+      { id: uuidv4(), title: 'Login Page', state: 'active' },
+      { id: uuidv4(), title: 'Dashboard Layout', state: 'pendedReview' },
+      { id: uuidv4(), title: 'Users Page', state: 'resolved' }
     ]
   },
   mutations: {
